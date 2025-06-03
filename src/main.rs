a5d77a5ccac53338ac7df99e796024d38166472f
matheus <matheussmoura@outlook.com>
2025-06-03 11:03:19 -0300

    output result file with the same name to the same folder

--- src/main.rs
+++ src/main.rs
@@ -17,18 +17,20 @@ mod token;
 fn main() -> std::io::Result<()> {
     let cur_dir = env::current_dir()?;
     let args = env::args().collect::<Vec<_>>();
-    let file = args.get(1).unwrap();
 
-    let path = cur_dir.join(file);
-    let code = std::fs::read_to_string(path)?;
+    let Some(file) = args.get(1) else {
+        return Err(std::io::Error::other("Error: no input file provided"));
+    };
+    
+    let in_file = cur_dir.join(file);
+    let out_file = in_file.with_extension("asm");
 
-    let out_file = args.get(2).map_or("out.asm", |x| x.as_str());
-    let out = cur_dir.join(out_file);
+    let code = std::fs::read_to_string(in_file)?;
 
     match Lexer::parse(&code) {
         Ok(tokens) => match Parser::new(&tokens).parse() {
             Ok(ast) => {
-                std::fs::write(out, Compiler::compile(ast))?;
+                std::fs::write(out_file, Compiler::compile(ast))?;
             }
             Err(err) => eprintln!("{file}:{err}"),
         },
